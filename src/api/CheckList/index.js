0e61f3fe9068e1f8a2da5d72fca4c5bd4009094e
gaowenzhao <510368603@qq.com>
2023-05-10 15:42:05 +0800

    111

--- src/api/CheckList/index.js
+++ src/api/CheckList/index.js
@@ -1,8 +1,8 @@
 import request from "@/utils/axios";
 
-export function checkListApi(data = {}) {
+export  function checkListApi(data = {}) {
   return request.post({
     url: "/cloudroom/api/cloudroomReport/list",
-    data,
+    data
   });
 }
